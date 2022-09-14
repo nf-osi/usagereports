29c2d4b07efee9d53c40d8cb4ce53efc288897ec
Anh Nguyet Vu <anngvu@gmail.com>
2022-09-14 08:30:30 -0600

    Add Synapse color palette for figs

---
+++ R/palette.R
@@ -0,0 +1,14 @@
+# Palette
+
+palette <- list(primary = "#125E81",
+                secondary = "#404B63",
+                highlight = "#6fbeb8",
+                gray1 = "#DCDCDC",
+                gray2 = "#BBBBBC",
+                gray3 = "gray",
+                purple1 = "#6d5d98",
+                danger = "#C9146C",
+                warning = "#F7A700",
+                success = "#2699A7")
+
+generic_palette <-c("#376b8b", "#e9b4ce", "#392965", "#f2d7a6", "#0e8177", "#bc590b")
