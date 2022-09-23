730f4f273b31193dc78add575327fcba9e576da7
Anh Nguyet Vu <anngvu@gmail.com>
2022-09-23 12:53:49 -0600

    Update palette generation and docs

--- R/palette.R
+++ R/palette.R
@@ -1,24 +1,47 @@
-# Palette
+#' Default palette
+#'
+#' @export
+default_palette <- function() {
+  list(primary = "#125E81",
+       secondary = "#404B63",
+       highlight = "#6fbeb8",
+       gray1 = "#DCDCDC",
+       gray2 = "#BBBBBC",
+       gray3 = "gray",
+       purple1 = "#6d5d98",
+       danger = "#C9146C",
+       warning = "#F7A700",
+       success = "#2699A7")
+}
 
-palette <- list(primary = "#125E81",
-                secondary = "#404B63",
-                highlight = "#6fbeb8",
-                gray1 = "#DCDCDC",
-                gray2 = "#BBBBBC",
-                gray3 = "gray",
-                purple1 = "#6d5d98",
-                danger = "#C9146C",
-                warning = "#F7A700",
-                success = "#2699A7")
+#' All-purpose Synapse palette
+#'
+#' Return max of 10 synapse colors.
+#' @export
+syn_palette <- function() {
 
-generic_palette <-c("#376b8b", "#e9b4ce", "#392965", "#f2d7a6", "#0e8177", "#bc590b", "#748dcd", "#af316c", "#aac3d4")
+  p <- c("#376b8b", "#e9b4ce", "#392965", "#f2d7a6", "#0e8177", "#bc590b", "#748dcd", "#af316c", "#aac3d4")
+  return(p)
+}
 
-project_status_palette <- c(`Pre-Synapse` = palette$gray2,
-                            Active= palette$highlight, 
-                            Completed= palette$purple1)
+#' Default project status palette
+#'
+#' @export
+project_status_palette <- function() {
+  p <- c(`Pre-Synapse` = default_palette $gray2,
+         Active= default_palette $highlight,
+         Completed= default_palette $purple1)
+  return(p)
+}
 
-data_status_palette <- c(`Pre-Synapse` = palette$gray1, 
-                         None = palette$purple1, 
-                         `Under Embargo`= palette$danger,
-                         `Partially Available`= palette$warning, 
-                         Available = palette$success)
+#' Default data status palette
+#'
+#' @export
+data_status_palette <- function() {
+  p <- c(`Pre-Synapse` = default_palette$gray1,
+         None = default_palette$purple1,
+         `Under Embargo`= default_palette$danger,
+         `Partially Available`= default_palette$warning,
+         Available = default_palette$success)
+  return(p)
+}
