7fab27beaff727e2c47fe504576f0c8a32c7ead3
Anh Nguyet Vu <anngvu@gmail.com>
2022-09-24 19:31:19 -0600

    Update supplement plot docs

--- R/supplement_figs.R
+++ R/supplement_figs.R
@@ -1,12 +1,14 @@
 #' Guage graph
-gauge_3x <- function(data, palette) {
-  
+#'
+#' @inheritParams plot_sankey_status
+plot_gauge_3x <- function(data, palette) {
+
   p <- ggplot(data, aes(fill = group, ymax = percentage, ymin = 0, xmax = 2, xmin = 1)) +
     geom_rect(aes(ymax=1, ymin=0, xmax=2, xmin=1), fill = palette$gray1 ) +
-    geom_rect() + 
+    geom_rect() +
     coord_polar(theta = "y",start=-pi/2) + xlim(c(0, 2)) + ylim(c(0,2)) +
     geom_text(aes(x = 0, y = 0, label = label, colour=group), size = 6.5, family = "Arial SemiBold") +
-    geom_text(aes(x=1.5, y=1.5, label = variable), family = "Arial", size=4.2) + 
+    geom_text(aes(x=1.5, y=1.5, label = variable), family = "Arial", size=4.2) +
     facet_wrap(~variable, ncol = 3) +
     theme_void() +
     scale_fill_manual(values = palette) +
