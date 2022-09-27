2b0e946edadfa27e03d27c9d6f9ab113d4d8ce86
Anh Nguyet Vu <anngvu@gmail.com>
2022-09-27 13:05:16 -0600

    Finalize GA fun to close issue #2

--- R/query_ga.R
+++ R/query_ga.R
@@ -1,6 +1,8 @@
 #' Convenience wrapper for Google Analytics
 #'
-#' Make sure already authenticated. Queries for pageviews and users.
+#' Wrapper to query for pageviews and users.
+#' Make sure already authenticated with `googleAnalyticsR::ga_auth()`.
+#'
 #' @param projects List of project syn ids, used for pagepath filters.
 #' @param date_range Start and end date.
 #' @import googleAnalyticsR
@@ -9,7 +11,7 @@ query_ga <- function(projects,
                      date_range) {
 
   ga_id <- 57135211 # Use "Synapse" view id
-  filter <- lapply(projects, function(x) dim_filter("pagePath", "BEGINS_WITH", paste0("/#!Synapse:", x)))
+  project_filters <- lapply(projects, function(x) dim_filter("pagePath", "BEGINS_WITH", paste0("/#!Synapse:", x)))
   filter_clause <- filter_clause_ga4(project_filters, "OR")
 
   pv <- google_analytics(ga_id,
@@ -17,5 +19,10 @@ query_ga <- function(projects,
                          metrics = c("pageviews", "users"),
                          dimensions = c("pagePath"),
                          dim_filters = filter_clause)
+
+  pv <- as.data.table(pv)
+  # Parse pagePath to synIds
+  pv[, synId := regmatches(pagePath, regexpr("syn[0-9]+", pagePath))]
+
   return(pv)
 }
