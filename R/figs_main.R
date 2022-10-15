929461b04f4fec79a8dcb5e9ca69cf2177229f62
Anh Nguyet Vu <anngvu@gmail.com>
2022-10-14 18:57:10 -0600

    Add donut chart for file tiers

--- R/figs_main.R
+++ R/figs_main.R
@@ -180,4 +180,29 @@ plot_bipartite <- function(data, hide_user_id = TRUE) {
 }
 
 
+#' Donut of file tiers
+#'
+#' Plot the restricted files as a proportion of total public files.
+#' Original data can come from `query_file_snapshot`.
+#' The tiers are `Public - Unrestricted` and `Public - Restricted`.
+#'
+#' @param data Table with columns `Tier` and `Count`.
+#' @param palette Needs two colors for tiers shown by default. See details.
+#' @export
+plot_donut_file_tiers <- function(data, palette) {
+  data$ymax <- cumsum(data$proportion)
+  data$ymin <- c(0, head(data$ymax, n = -1))
+  data$label <- glue::glue("{data$tier}\n{data$count} files")
+  data$labelpos <- (data$ymax + data$ymin) / 2
+  p <- ggplot(data, aes(ymax = ymax, ymin = ymin, xmax=4, xmin=3, fill = tier)) +
+    geom_rect() +
+    geom_text(x = 4.5, aes(y = labelpos, label = label, color = tier), size = 5, show.legend = F) +
+    coord_polar(theta = "y") +
+    scale_fill_manual(values = palette, name = "Data Tier") +
+    xlim(c(1,4)) +
+    theme_void()
+
+  return(p)
+}
+
 
