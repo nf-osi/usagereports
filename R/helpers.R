4815e7b77995f09174c80d02f4844c69b487f0a4
Anh Nguyet Vu <angvu@ip-192-168-0-104.ec2.internal>
2023-09-09 15:08:04 -0600

    Add helper.R

---
+++ R/helpers.R
@@ -0,0 +1,17 @@
+#' Write a default config yaml
+#' 
+#' Create a `config.yml` to store datawarehouse connection parameters if you don't already one.
+#' 
+dw_config <- function(host = "",
+                      port = 3306,
+                      username = "",
+                      password = "") {
+  writeLines(
+    c(glue::glue("host: {host}"),
+      glue::glue("port: {port}"),
+      "db: warehouse", # this is always warehouse
+      glue::glue("username: {username}"),
+      glue::glue("password: {password}")),
+    con = "config.yml")
+  message("Saved `config.yml`. If you are in a git repo, please add it to .gitignore.")
+} 
