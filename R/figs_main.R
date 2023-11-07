35e6943ef2e2269880dcb891c4532bfc7381977d
Anh Nguyet Vu <anngvu@gmail.com>
2023-11-07 08:45:04 -0700

    Switch to graphviz for flowchart for greater control

--- R/figs_main.R
+++ R/figs_main.R
@@ -228,8 +228,8 @@ plot_donut_file_tiers <- function(data, palette) {
 
 #' Project breakdown flowchart template
 #'
-#' Generate a mermaid.js flowchart breaking down projects into groupings based on data status.
-#' Can use as a starting point depending on the DCC.
+#' Generate a GraphViz flowchart breaking down projects into groupings based on data status.
+#' Intended for use as a starting point.
 #'
 #' @param total Total number.
 #' @param available Number available from total.
@@ -238,7 +238,7 @@ plot_donut_file_tiers <- function(data, palette) {
 #' @param under_embargo Number under embargo.
 #' @param pending Number where data is still being generated.
 #' @param not_expected Number where data not expected.
-#' @param orientation Chart orientation (defaults to "TD", top-down).
+#' @param direction Chart orientation (defaults to "TD" top-down, alternative is "LR" left-right).
 #' @export
 #'
 project_flowchart_template <- function(
@@ -249,37 +249,65 @@ project_flowchart_template <- function(
                                    under_embargo,
                                    pending,
                                    not_expected,
-                                   orientation = c("TD", "LR")) {
+                                   direction = c("TD", "LR")) {
 
-  orientation <- match.arg(orientation)
-  theme <- "%%{init: {'themeVariables': { 'primaryColor': '#125e81','edgeLabelBackground': 'white' }}}%%"
-
+  direction <- match.arg(direction)
   glue::glue(
-  'flowchart {orientation}
-    {theme}
+  'digraph {
 
-    classDef Blue fill:#125e81,color:#fff,stroke-width:0px
-    classDef Green fill:#00c87a,color:#fff,stroke-width:0px
-    classDef Red fill:#af316c,color:#fff,stroke-width:0px
-    classDef Pink fill:#e9b4ce,color:black,stroke-width:0px
-    classDef Purple fill:#392965,color:#fff,stroke-width:0px
-    classDef Yellow fill:#f2d7a6,color:black,stroke-width:0px
-    classDef Gray fill:#636E83,color:white,stroke-width:0px
+  rankdir={{direction}};
 
-    Total["Total projects</br>(n={total})"]:::Purple
-    Available["Data Partially Available</br>or Available</br>(n={available})"]:::Blue
-    Reprocessed["Data Reprocessed</br>(n={reprocessed})"]:::Green
-    DataUnreleased["Data Unreleased</br>(n={unreleased})"]:::Red
-    UnderEmbargo["Data Under Embargo</br>(n={under_embargo})"]:::Pink
-    DataPending["Data Pending</br>(n={pending})"]:::Yellow
-    DataNotExpected["Data Not Expected</br>(n={not_expected})"]:::Gray
+  node [shape="box" fontname="Helvetica" style=filled fontcolor="white"];
+  edge [fontname="Helvetica"];
+  {rank = same; Available; DataUnreleased; DataNotExpected;}
 
-    Total-->Available
-    Available-->Reprocessed
-    Total-->DataUnreleased
-    DataUnreleased-->UnderEmbargo
-    DataUnreleased--->DataPending
-    Total-->DataNotExpected
-    ')
+  Total[label = "Total projects\n(n={{total}})" fillcolor="#392965"]
+  Available[label = "Data Partially Available\nor Available\n(n={{available}})" fillcolor="#125e81"]
+  Reprocessed[label = "Data Reprocessed\n(n={{reprocessed}})" fillcolor="#00c87a"]
+  DataUnreleased[label = "Data Unreleased\n(n={{unreleased}})" fillcolor="#af316c"]
+  UnderEmbargo[label = "Data Under Embargo\n(n={{under_embargo}})" fillcolor="#e9b4ce" fontcolor="black"]
+  DataPending[label = "Data Pending\n(n={{pending}})" fillcolor="#f2d7a6" fontcolor="black"]
+  DataNotExpected[label = "Data Not Expected\n(n={{not_expected}})" fillcolor="#636E83"]
+
+  Total->Available;
+  Available->Reprocessed;
+  Total->DataUnreleased;
+  DataUnreleased->UnderEmbargo;
+  DataUnreleased->DataPending;
+  Total->DataNotExpected;
+  UnderEmbargo->Reprocessed;
+  }', .open = "{{", .close = "}}")
+
 }
+
+#' Add additional component to flowchart
+#'
+#' Graphviz partial template for representing a component with
+#' qualification, acceptance, and in-preparation states.
+#'
+#' @param qualify Number for qualification of something.
+#' @param accepted Number for accepted.
+#' @param in_preparation Summary number for in preparation.
+#' @param component Prefix for component id, defaults to "cBP" for cBioPortal
+#' @param connector Connecting edge if adding this to an existing plot; use NULL for stand-alone plot.
+#' @export
+flowchart_component_template <- function(
+    qualify,
+    accepted,
+    in_preparation,
+    component = "cBP",
+    connector = "Reprocessed->cBP_qualify;"
+) {
+
+  glue::glue('
+  {component}_qualify[label = "Qualify for cBioPortal\n(n={qualify})" fillcolor="black"]
+  {component}_accepted[label = "Accepted to\ncBioPortal\n(n={accepted})" fillcolor="#748dcd"]
+  {component}_prep[label = "Preparing for\ncBioPortal\n(n={in_preparation})" fillcolor="#748dcd"]
+
+  {connector}
+  {component}_qualify->{component}_accepted;
+  {component}_qualify->{component}_prep;
+  ')
+}
+
 
