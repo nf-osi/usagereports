7c2b8c3fa238cb558ceb752a0feb87ed3b27f666
Anh Nguyet Vu <anngvu@gmail.com>
2023-11-15 09:36:39 -0700

    Develop circular packing fig

--- R/figs_circular.R
+++ R/figs_circular.R
@@ -37,3 +37,35 @@ plot_pie <- function(data) {
     coord_polar("y", start=0) +
     theme_void()
 }
+
+
+#' Hierarchical circle packing
+#'
+#' This is similar to a treemap and is useful for representing proportions of hierarchical entities (ontology classes and subclasses).
+#'
+#' @param nodes Nodes data. See `simd_class_count` to get example.
+#' @param edges Edges data. See `simd_class_count` to get example.
+#' @param seed Optional, seed for layout.
+#' @param background Optional, background color.
+#' @export
+fig_class_count <- function(nodes,
+                            edges,
+                            seed = 31,
+                            background = "#201037") {
+
+  set.seed(seed)
+
+  # Graph obj and graph
+  graph <- tidygraph::tbl_graph(nodes, edges)
+
+  # Layering manually because https://github.com/thomasp85/ggraph/issues/230
+  ggraph::ggraph(graph, 'circlepack', weight = size) +
+    ggraph::geom_node_circle(aes(filter = level==1, fill = I(fill)), color = "white", size = 0.25) +
+    ggraph::geom_node_circle(aes(filter = level==2, fill = I(fill)), color = background, size = 0.25) +
+    ggplot2::coord_fixed() +
+    ggplot2::theme(legend.position = "FALSE") +
+    ggplot2::theme_void() +
+    ggraph::geom_node_label(aes(label = name), repel = TRUE)
+
+}
+
